292478a0eccb091e692e22749a9e55381627a9e6
Boris <bgrebennikovv@gmail.com>
2025-01-05 21:55:58 +0400

    https

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://backend:8000/api/:path*',
-            }
-        ]
-    }
 };
 
 export default nextConfig;
